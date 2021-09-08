a44d816155114ae0a9b9ba43e1cab96f01927173
CamilaRanniele <rannielecamsil@gmail.com>
2021-09-08 14:43:39 -0300

    Adicionando exercicio do dia

---
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-1-javascript-dom-e-seletores/script.js
@@ -0,0 +1,8 @@
+document.querySelectorAll("#header-container")[0].style.backgroundColor = "#00b069"
+document.querySelectorAll(".emergency-tasks h3")[0].style.backgroundColor = "#a500f3"
+document.querySelectorAll(".emergency-tasks h3")[1].style.backgroundColor = "#a500f3"
+document.querySelectorAll(".emergency-tasks")[0].style.backgroundColor = "#ff9f84";
+document.querySelector(".no-emergency-tasks").style.backgroundColor = "#f9db5e"
+document.querySelectorAll(".no-emergency-tasks h3")[0].style.backgroundColor = "black";
+document.querySelectorAll(".no-emergency-tasks h3")[1].style.backgroundColor = "black";
+document.querySelector("footer").style.backgroundColor = "#003533"
