01eadc7c00ede0f2ecc7fbb364a8aad9e9ebfb2e
CamilaRanniele <rannielecamsil@gmail.com>
2021-09-20 15:00:45 -0300

    Adicionando modificacoes

---
+++ fundamentos/bloco-6-html-e-css-forms-flexbox-e-responsivo/dia-1-html-css-forms/script.js
@@ -0,0 +1,42 @@
+let estados = [
+  'AC - Acre',
+  'AL - Alagoas',
+  'AP - Amapá',
+  'AM - Amazonas',
+  'BA - Bahia',
+  'CE - Ceará',
+  'DF - Distrito Federal',
+  'ES - Espírito Santo',
+  'GO - Goías',
+  'MA - Maranhão',
+  'MT - Mato Grosso',
+  'MS - Mato Grosso do Sul',
+  'MG - Minas Gerais',
+  'PA - Pará',
+  'PB - Paraíba',
+  'PR - Paraná',
+  'PE - Pernambuco',
+  'PI - Piauí',
+  'RJ - Rio de Janeiro',
+  'RN - Rio Grande do Norte',
+  'RS - Rio Grande do Sul',
+  'RO - Rondônia',
+  'RR - Roraíma',
+  'SC - Santa Catarina',
+  'SP - São Paulo',
+  'SE - Sergipe',
+  'TO - Tocantins'
+  ];
+function addStates () {
+  const selectState = document.querySelector('#select-state');
+  for (let index = 0; index < estados.length; index += 1) {
+    const state = estados[index];
+    const newElement = document.createElement('option');
+    newElement.innerText = state;
+    selectState.appendChild(newElement);
+  }
+}
+
+window.onload = () => {
+addStates();
+}
